56856d106140c63eac550afc8552012e91ae58cc
Shaxshax-Mahat <abdulrahmanmahat0@gmail.com>
2025-09-16 16:31:07 +0300

    Complete task1

--- task1.py
+++ task1.py
@@ -3,3 +3,20 @@ Write a program that takes as input the speed of a car e.g 80. If the speed is l
 For example, if the speed is 80, it should print: “Points: 2”. If the driver gets more than 12 points, the function should print: “License suspended”.
 
 '''
+
+def speed_checker(car_speed):
+    speed_limit = 70
+    
+    if car_speed < speed_limit:
+        print("Ok")
+    else:
+        excess_speed = car_speed - speed_limit
+        points = excess_speed // 5
+        
+        if points > 12:
+            print("License suspended")
+        else:
+            print("Points: " + str(points))
+
+car_speed = int(input("Car speed is: "))
+speed_checker(car_speed)
